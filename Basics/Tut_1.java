f32d5f14c42bfa413d41ea4b0056d20d2179c340
Ayush <ayushjha12345687@gmail.com>
2026-01-17 20:04:36 +0530

    Day - 1 code

---
+++ Basics/Tut_1.java
@@ -0,0 +1,9 @@
+class Tut_1{
+
+         public static void main (String arr[]){
+                   System.out.println("Hello World !!");
+                   System.out.print(" Hellow World Without 'ln' "); // here we did not use 'ln' ln is for make print words and point to next line without 'ln' its just print in same line 
+
+}
+
+}
