fc60ff819c4e645e5adfec412612d415a8ad9c25
Ayush <ayushjha12345687@gmail.com>
2026-01-17 21:26:06 +0530

    Day 1 - Operators

---
+++ Basics/operators.java
@@ -0,0 +1,36 @@
+class operators{
+
+        public static void main(String arr[]){
+                   int a, b;
+                   a=40;
+                   b=50;
+                   int sum = a + b;
+                  System.out.print("sum of a and b is = ");
+                  System.out.println(sum);
+                  
+                  int g, h;
+                  g = 20;
+                  h = 10;
+                  int subtraction = g - h;
+                  System.out.print("subraction of g and h is =");
+                  System.out.println(subtraction);
+                  
+                  int c, d;
+                   c=4;
+                   d=5;
+                   int multi = c * d;
+                  System.out.print("multiplication of c and d is = ");
+                  System.out.println(multi);
+
+                  int e, f;
+                  e=10;
+                  f = 2;
+                  int division = e / f;
+                  System.out.print("division of e and f is = ");
+                  System.out.println(division);
+                  
+
+}
+
+}
+
