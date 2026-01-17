170ba09352a1ca655b419dbb7364418efc1506ad
Ayush <ayushjha12345687@gmail.com>
2026-01-17 20:51:31 +0530

    Day 1 - variable

---
+++ Basics/variable.java
@@ -0,0 +1,16 @@
+class variable{
+
+public static void main (String arr[]){
+
+           int a;
+           a = 50;          
+          System.out.println(a);
+          a = 40;
+          System.out.println(a);
+          
+          String name="Ayush";
+          System.out.println(name);
+
+}
+
+}
